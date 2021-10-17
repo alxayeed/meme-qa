fbd8cb779448c635f188fd4a3700ebd0c852d817
alxayeed <alxayeed@gmail.com>
2021-10-17 18:06:52 +0600

    meme is showing in the webpage

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: avoid_print, unused_field, unnecessary_null_comparison
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:http/http.dart' as http;
+import 'package:meme_qa/models/answer.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -8,6 +13,31 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  /// text editing controller to control tesxxt fileds
+  final TextEditingController _questionFieldController =
+      TextEditingController();
+
+  // Store current answer object
+  Answer? _currentAnswer;
+
+  // process answer response
+  _handleGetAnswer() async {
+    String url = 'https://yesno.wtf/api';
+    try {
+      http.Response response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200 && response.body != null) {
+        Answer answer = Answer.fromJson(response.body);
+
+        setState(() {
+          _currentAnswer = answer;
+        });
+      }
+    } catch (e, stacktrace) {
+      print(e);
+      print(stacktrace);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,10 +52,38 @@ class _HomepageState extends State<Homepage> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          if (_currentAnswer != null)
+            Stack(
+              children: [
+                Container(
+                  height: 250,
+                  width: 400,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(_currentAnswer!.image))),
+                ),
+                Positioned.fill(
+                    bottom: 20,
+                    right: 20,
+                    child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Text(_currentAnswer!.answer.toUpperCase(),
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold))))
+              ],
+            ),
+          const SizedBox(
+            height: 40,
+          ),
           SizedBox(
             width: 0.4 * MediaQuery.of(context).size.width,
-            child: const TextField(
-              decoration: InputDecoration(
+            child: TextField(
+              controller: _questionFieldController,
+              decoration: const InputDecoration(
                 labelText: "Ask what's on your mind",
                 border: OutlineInputBorder(),
               ),
@@ -39,7 +97,7 @@ class _HomepageState extends State<Homepage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               ElevatedButton(
-                onPressed: () {},
+                onPressed: _handleGetAnswer,
                 child: const Text("Ask Me"),
                 style: ElevatedButton.styleFrom(
                     primary: Colors.deepPurple,
